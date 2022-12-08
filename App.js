002fa0a91a9aa947727793d4d9f3589a30f77c29
Dương Văn Thương <thuongvan@MBP-cua-Duong.lan>
2022-12-09 00:48:07 +0700

    Flashlist applied and ios icon and splashscreen

---
+++ App.js
@@ -0,0 +1,127 @@
+import 'react-native-gesture-handler';
+import React, { useState } from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import Task from './Task';
+import SplashScreen from 'react-native-splash-screen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+
+export default function App() {
+
+  const [task, setTask] = useState('');
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
+  const completeTask = () => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice("", 1);
+    setTaskItems(itemsCopy);
+  };
+  setTimeout (() => {
+    SplashScreen.hide();
+  },1000);
+  
+  return (
+    <View style={styles.container}>
+      <View
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View style={styles.tasksWrapper}>
+          <Text style={styles.sectionTitle}>Today's tasks</Text>
+          <View style={styles.items}>
+                  <FlatList
+                    data={taskItems}
+                    renderItem={({ item }) => (
+                      <Task
+                        text={item}
+                        completeTask={() => completeTask(item)
+                        }
+                      />
+                    )}
+                  />          
+          </View>
+        </View>
+      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "white",
+    borderRadius: 60,
+    borderColor: "gray",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "white",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "gray",
+    borderWidth: 1,
+  },
+});
---
+++ Task.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
+import CheckBox from "@react-native-community/checkbox";
+
+const Task = (props) => {
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <TextInput style={styles.itemText}>{props.text}</TextInput>
+      </View>
+      <TouchableOpacity
+        style={styles.delButton}
+        onPress={props.completeTask}
+      >
+        <Text style={styles.delText}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#FFF",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: "80%",
+    fontSize: 20,
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: "#55BCF6",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+  delButton: {
+    backgroundColor: "#FF6883",
+    height: 20,
+    width: 50,
+    borderRadius: 5,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  delText: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});
+
+export default Task;
