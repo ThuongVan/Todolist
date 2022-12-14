0bf034ae61e804bce14450786e6d5ec4a774d6dd
Dương Văn Thương <thuongvan@192.168.1.12>
2022-12-14 18:26:29 +0700

    Navigation + Custom Font

--- android/app/src/main/java/com/testing/MainApplication.java
+++ android/app/src/main/java/com/testing/MainApplication.java
@@ -36,7 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
-      };
+      };`
 
   private final ReactNativeHost mNewArchitectureNativeHost =
       new MainApplicationReactNativeHost(this);
